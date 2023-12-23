4b8ea6c484c8e44c4bf3b95e95b2dd5c59420796
midiareshadi <midia.reshadi@gmail.com>
2023-12-23 17:18:25 +0000

    updating fa test

--- src/test/scala/FulladderTest.scala
+++ src/test/scala/FulladderTest.scala
@@ -1,4 +1,3 @@
-
 // package package_name
 
 import chisel3._
@@ -13,9 +12,9 @@ class FullAdderTests extends AnyFlatSpec with ChiselScalatestTester {
         for (a <- 0 until 2) {
             for (b <- 0 until 2) {
                 for (cin <- 0 until 2) {
-                    dut.io.a.poke(a.U)
-                    dut.io.b.poke(b.U)
-                    dut.io.cin.poke(cin.U)
+                    dut.io.a.poke(a.B)
+                    dut.io.b.poke(b.B)
+                    dut.io.cin.poke(cin.B)
                     dut.clock.step()
                     // dut.io.cout.expect(dut.io.cout.peekInt())
                     println("a=" + dut.io.a.peekInt() + "," + " b=" 
